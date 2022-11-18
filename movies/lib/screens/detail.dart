2ee5fa7f400c1e2cc5bfb82980c2bbbf72f04dfd
vlSolrac <Carlosa.velaluna@gmail.com>
2022-11-17 23:03:34 -0600

    detail

--- movies/lib/screens/detail.dart
+++ movies/lib/screens/detail.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
 
+import 'package:movies/models/models.dart';
+import 'package:movies/screens/screens.dart';
+import '../widgets/widgets.dart';
 
 class DetailScreen extends StatelessWidget {
- 
 //Parametes.
 
   const DetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-  
+    final size = MediaQuery.of(context).size;
+    // get arguments
+    final Movie? movie = ModalRoute.of(context)?.settings.arguments as Movie?;
     //global variables.
-  
-    return const Scaffold(
-      body: Center(
-         child: Text('DetailScreen'),
-      ),
-    );
+
+    return movie == null
+        ? const NotFoundScreen()
+        : Scaffold(
+            body: CustomScrollView(
+            slivers: [
+              CustomAppBar(
+                size: size,
+                title: movie.title,
+                imgUrl: movie.fullBackDropImg,
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  PosterAndTitle(
+                    size: size,
+                    imgUrl: movie.fullPosterImg,
+                    title: movie.title,
+                    originalTitle: movie.originalTitle,
+                    rating: movie.voteAverage.toString(),
+                    id: movie.id,
+                  ),
+                  Overview(overview: movie.overview),
+                  Overview(overview: movie.overview),
+                  CastingSlider(idMovie: movie.id),
+                  const SizedBox(height: 20)
+                ]),
+              )
+            ],
+          ));
   }
 }
--- movies/lib/screens/screens.dart
+++ movies/lib/screens/screens.dart
@@ -1,3 +1,6 @@
+export 'package:movies/screens/not_found.dart';
+
 export 'package:movies/screens/detail.dart';
 
 export 'package:movies/screens/home.dart';
+ 
